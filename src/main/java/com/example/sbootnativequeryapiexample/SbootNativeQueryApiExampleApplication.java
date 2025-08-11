8c5751caaebdb2ee254100618aeb9b63b0f0a954
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-11 16:41:04 -0400

    Primer commit

---
+++ src/main/java/com/example/sbootnativequeryapiexample/SbootNativeQueryApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbootnativequeryapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootNativeQueryApiExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbootNativeQueryApiExampleApplication.class, args);
+  }
+
+}
